38e0c3578f855c4a8f9341800817847468b1c9a3
andyrueda439 <ajrueda@uamv.edu.ni>
2026-01-10 16:32:50 -0600

    Actualiza variable

--- Basic/00-variable.js
+++ Basic/00-variable.js
@@ -1,2 +1,8 @@
 var Helloworld = "Hola hamilton, estoy probando el javascript";
 console.log(Helloworld);
+
+let Helloworld2 = "Hola hamilton, estoy probando el javascript con let";
+console.log(Helloworld2);
+
+const Helloworld3 = "Hola hamilton, estoy probando el javascript con const";
+console.log(Helloworld3);
