f5c85da2b119960bd48d535272d763fe4a81ac09
andyrueda439 <ajrueda@uamv.edu.ni>
2026-01-10 16:33:55 -0600

    Helloworld actualizado

--- Basic/00-Helloworld.js
+++ Basic/00-Helloworld.js
@@ -1,1 +1,8 @@
-console.log("Hola hamilton, estoy probando el javascript")
+console.log("Hola hamilton, estoy probando el javascript") //Comentario
+
+/*console. log("Hola hamilton, estoy probando el javascript")
+console. log("Hola hamilton, estoy probando el javascript")
+console. log("Hola hamilton, estoy probando el javascript")
+console. log("Hola hamilton, estoy probando el javascript")*/
+
+console.log("Hola hamilton, estoy probando el javascript") //que sobre que se pueden hacer operaciones tambien 
