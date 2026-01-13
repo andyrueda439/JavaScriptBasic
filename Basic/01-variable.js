591372b21aeb964d41acf366a6052fa98ec30420
andyrueda439 <ajrueda@uamv.edu.ni>
2026-01-12 18:26:39 -0600

    Actualizacion en variables

---
+++ Basic/01-variable.js
@@ -0,0 +1,8 @@
+var helloWorld = "Hola hamilton, estoy probando el javascript";
+console.log(helloWorld);
+
+let helloWorld2 = "Hola hamilton, estoy probando el javascript con let";
+console.log(helloWorld2);
+
+const helloWorld3 = "Hola hamilton, estoy probando el javascript con const";
+console.log(helloWorld3);
